d26b3e1aca9dfcc89192b28c32776d72efa104e2
mdfaiz903 <mdfaiz.nub@gmail.com>
2024-03-29 07:04:48 +0600

    Use bootstrap in react

--- first_project/src/App.jsx
+++ first_project/src/App.jsx
@@ -1,14 +1,20 @@
 import Condition_ren_IIF from "./component/Condition_ren_IIF";
 import Conditional_rendering from "./component/Conditional_rendering";
 import Map_fun from "./component/Map_fun";
-
-
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+import { useRef } from "react";
 
 
 const App = () => {
   function SubForm(A){
     A.preventDefault();
     alert("No Reload");
+  }
+
+  let myheadline = useRef();
+  const text_int = ()=>{
+    myheadline.current.innerText = "Hellow Bangladesh"
   }
   return (
     <div>
@@ -30,6 +36,11 @@ const App = () => {
         <input type="text" name="" placeholder="Name" id="" />
         <button type="submit">Submit</button>
       </form>
+      <hr />
+  
+      <h1 className="text-danger" ref={myheadline} ></h1>
+      <button onClick={text_int} type="submit">Click</button>
+
     </div>
   );
 };
